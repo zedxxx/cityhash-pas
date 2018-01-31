d6421bfc0207ec7ca81dcc43666c3cdf16150220
zed <starmen@tut.by>
2018-01-31 13:15:05 +0300

    Config.h and minor modifications to build .dll with MinGW-w64

--- city.cc
+++ city.cc
@@ -47,7 +47,7 @@ static uint32 UNALIGNED_LOAD32(const char *p) {
   return result;
 }
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__GNUC__)
 
 #include <stdlib.h>
 #define bswap_32(x) _byteswap_ulong(x)
--- city.h
+++ city.h
@@ -73,7 +73,7 @@ typedef std::pair<uint64, uint64> uint128;
 
 inline uint64 Uint128Low64(const uint128& x) { return x.first; }
 inline uint64 Uint128High64(const uint128& x) { return x.second; }
-
+extern "C" {
 // Hash function for a byte array.
 uint64 CityHash64(const char *buf, size_t len);
 
@@ -95,7 +95,7 @@ uint128 CityHash128WithSeed(const char *s, size_t len, uint128 seed);
 
 // Hash function for a byte array.  Most useful in 32-bit binaries.
 uint32 CityHash32(const char *buf, size_t len);
-
+}
 // Hash 128 input bits down to 64 bits of output.
 // This is intended to be a reasonably good hash function.
 inline uint64 Hash128to64(const uint128& x) {
